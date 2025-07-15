1d4598dddd17d30281669a0fb3180e6be876e4d9
MaSidrenko <nexon863@gmail.com>
2025-07-16 01:23:17 +0300

    img load DONE

--- 02-JavaScript/05-functions/functions.js
+++ 02-JavaScript/05-functions/functions.js
@@ -9,9 +9,45 @@ function Power(base, exponent) {
 }
 function SwitchBackground() {
     let switchButton = document.getElementById('background_switch');
+    let secondSwitchButton = document.getElementById('background_switch_second');
     let delay = Number(document.getElementById('delay').value);
     document.body.style.transition = `background-color ${delay}s, color ${delay}s`;
-    switchButton.style.transition = `background-image ${delay}s`;
+    // switchButton.style.transition = `background-image ${delay}s`;
+      switchButton.style.transition = `opacity ${delay}s`;
+    secondSwitchButton.style.transition = `opacity ${delay}s`;
     document.body.className = document.body.className === "light" ? "dark" : "light";
     console.log(switchButton.attributes);
+}
+
+document.addEventListener("mousemove", function(event){
+    let x = event.clientX;
+    let y = event.clientY;
+    document.getElementById("mouse").innerHTML = `X = ${x}, Y = ${y}`;
+});
+
+function setImage() {
+    // let img = new Image();
+
+    // let file_name = document.getElementById("image-file").value;
+    // console.log(file_name);
+    // console.log(file_name.split('\\'));
+    // let splited_filename = file_name.split('\\');
+    // // document.getElementById("photo").attributes.src = splited_filename
+    // // [splited_filename.lenght - 1]
+    // img.src = splited_filename[splited_filename.length - 1];
+    // console.log(splited_filename[splited_filename.length - 1]);
+    // document.getElementById("photo").attributes.src = splited_filename[splited_filename.length - 1];
+    // console.log(document.getElementById("photo").src)
+    let file_name = document.getElementById("image-file");
+    let = file = file_name.files[0];
+    console.log(file);
+    console.log("========================");
+    let imageUrl = URL.createObjectURL(file);
+    console.log(imageUrl);
+    console.log("========================");
+    let imgElement = document.getElementById("photo");
+    console.log(imgElement);
+    console.log("========================"); 
+       imgElement.src = imageUrl;
+
 }
